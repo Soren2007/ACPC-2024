8a1985042e6a7d0625e7948f3b3eb075b8d8b00c
Soren Shamlou <99844121+Soren2007@users.noreply.github.com>
2024-10-20 20:19:27 +0330

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,42 +1,38 @@
 """
-ACPC 1
+ACPC 1 Q1
 """
-
-__author__ = "SORENSHAMLOU"
-
-__email__ = "sorenShamloo.programmer@gmail.com"
-
-__mobile__ = "09932062665"
-
 from numpy import (
     zeros,
     int32
 )
 
 try:
-    home = int(input(f"Home Number >>> "))
+    n = int(input(f"n >>> "))
 except ValueError:
     print("ValueError")
     exit(0)
 
-if 0 <= home <= 100:
-    sum_bomb = 0
-    array = zeros(home, int32)
-    x = 1
-    for i in range(home):
+if 0 <= n <= 100:
+    sum_number = 0
+    array = zeros(
+        n,
+        int32
+    )
+
+    i = 0
+    while sum(array) < 2 * n:
         x = i+1 # خانه بعدی
         y = i-1 # خانه قبلی
         if y < 0:
-            y = home-1
-        if x >= home:
+            y = n-1
+        if x >= n:
             x = 0
-        if array[i] == 2:
-            continue
+        if n <= i:
+            i = n-1
         array[i]+=2
         array[x]+=1
         array[y]+=1
-        sum_bomb+=1
-        if sum(array) >= 2 * home:
-            break
+        sum_number+=1
+        i+=2
 
-print(f"sum bombs : {sum_bomb}")
+print(f"{sum_number}")
